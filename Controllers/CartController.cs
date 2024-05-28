3ce32ccb6ffe79ec79ec838340b2be8196cde21f
ThienMenh <dinhthienmenh1505@gmail.com>
2024-05-28 23:22:10 +0700

    Fix: Book detail page

---
+++ Controllers/CartController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyApp.Namespace
+{
+    public class CartController : Controller
+    {
+        // GET: CartController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
