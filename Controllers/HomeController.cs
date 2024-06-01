f7032a9a2f8703ba8d517c3f5e8049754fe1f0c7
ThienMenh <dinhthienmenh1505@gmail.com>
2024-06-01 18:51:18 +0700

    Merge branch 'main' of https://github.com/Menh1505/Online-Library

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,6 +18,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult detail()
+    {
+        return View();
+    }
+
     public IActionResult Privacy()
     {
         return View();
