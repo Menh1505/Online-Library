60e6c7443d92b74895a221edaf5ffb88ecc5068b
phint24 <nguyentanphi24022005@gmail.com>
2024-06-18 17:45:39 +0700

    create: _AdminLayout.cshtml

---
+++ Views/_AdminLayout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OnlineLibrary.Views
+{
+    public class _AdminLayoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
