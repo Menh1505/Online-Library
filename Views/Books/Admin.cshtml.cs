a7ecf48b618d9f1bbd037b782ca75ca563e85f93
phint24 <nguyentanphi24022005@gmail.com>
2024-06-18 16:30:24 +0700

    create: Admin.cshtml

---
+++ Views/Books/Admin.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OnlineLibrary.Views.Books
+{
+    public class AdminModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
